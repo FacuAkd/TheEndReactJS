5a1e3053cb1bf70b15694d0bf887cc36d755ba53
Facundo Zamora <zamorafe@hotmail.com>
2022-12-29 23:02:40 -0300

    first commit

--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -7,7 +7,7 @@ const Redes = () => {
       <a href="https://www.facebook.com/" class="icon icon-facebook" target="_blank"></a>
       <a href="https://twitter.com/" class="icon icon-twitter" target="_blank"></a>
       <a href="https://www.instagram.com/" class="icon icon-instagram" target="_blank"></a>
-      <a href="https://github.com/FacuAkd/The_End-Facundo_Zamora" class="icon icon-github" target="_blank"></a>
+      <a href="https://github.com/FacuAkd/TheEndReactJS" class="icon icon-github" target="_blank"></a>
     </div>
   )
 }
