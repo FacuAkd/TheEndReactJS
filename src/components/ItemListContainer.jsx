ccbe60d9414bb180fa337cbc4f2d52a6a75abd8a
Facundo Zamora <zamorafe@hotmail.com>
2022-12-29 23:01:40 -0300

    first commit

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -6,7 +6,7 @@ const ItemListContainer = () => {
                 <div class="row">
                     <div class="col">
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item list-group-item-dark"><a class="nav-link active" aria-current="page" href="https://github.com/FacuAkd/ReactJs"target="_blank">GITHUB</a></li>
+                        <li class="list-group-item list-group-item-dark"><a class="nav-link active" aria-current="page" href="https://github.com/FacuAkd/TheEndReactJS"target="_blank">GITHUB</a></li>
                         <li class="list-group-item list-group-item-info"><a class="nav-link active" aria-current="page" href="https://es-la.facebook.com/"target="_blank">FACEBOOK</a></li>
                         <li class="list-group-item list-group-item-danger"><a class="nav-link active" aria-current="page" href="https://www.instagram.com/"target="_blank">INSTAGRAM</a></li>
                         <li class="list-group-item list-group-item-primary"><a class="nav-link active" aria-current="page" href="https://twitter.com/?lang=es"target="_blank">TWITTER</a></li>
